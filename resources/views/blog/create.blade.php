47c2e705d67d35ff11e0b1fa43bc822cd3a4537a
vono <vani4kamaksimov.2006@gmail.com>
2025-09-23 22:39:04 +0300

    3.2

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -1,5 +1,5 @@
 
 @extends('layouts.app')
 @section('content')
-<h1>About</h1>
+<h1>Create Blog Post</h1>
 @endsection
--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -1,5 +1,4 @@
-
 @extends('layouts.app')
 @section('content')
-<h1>About</h1>
+<h1>Blog Post #{{ $id }}</h1>
 @endsection
